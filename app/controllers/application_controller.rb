89dcda87a3246c56431a2779e09aae90b56889b3
7022Andre <7022Andre@users.noreply.github.com>
2017-05-22 09:51:19 -0700

    Checkpoint 27 complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  include SessionsHelper
+	protect_from_forgery with: :exception
+	include SessionsHelper
+
+	def require_sign_in
+		unless current_user
+			flash[:alert] = "Please login first."
+			redirect_to new_session_path
+		end
+	end
 end
